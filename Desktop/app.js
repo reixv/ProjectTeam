32747fd92e5689d0d01c7bee38929a263be80b04
roaaalhammad <roaaalhaamad@gmail.com>
2024-04-07 08:29:48 +0300

    Names of variables

--- Desktop/app.js
+++ Desktop/app.js
@@ -1,11 +1,11 @@
-const hamburger = document.querySelector('.header .nav-bar .nav-list .hamburger');
-const mobile_menu = document.querySelector('.header .nav-bar .nav-list ul');
+const nib = document.querySelector('.header .nav-bar .nav-list .nib');
+const menu = document.querySelector('.header .nav-bar .nav-list ul'); 
 const menu_item = document.querySelectorAll('.header .nav-bar .nav-list ul li a');
 const header = document.querySelector('.header.container');
 
-hamburger.addEventListener('click', () => {
-	hamburger.classList.toggle('active');
-	mobile_menu.classList.toggle('active');
+nib.addEventListener('click', () => {
+	nib.classList.toggle('active');
+	menu.classList.toggle('active');
 });
 
 document.addEventListener('scroll', () => {
@@ -19,7 +19,7 @@ document.addEventListener('scroll', () => {
 
 menu_item.forEach((item) => {
 	item.addEventListener('click', () => {
-		hamburger.classList.toggle('active');
-		mobile_menu.classList.toggle('active');
+		nib.classList.toggle('active');
+		menu.classList.toggle('active');
 	});
 });
